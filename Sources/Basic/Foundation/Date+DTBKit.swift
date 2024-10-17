b4784bb63cda562c3dfccca169c58d162d036e0f
moonShadow <331614794@qq.com>
2024-10-17 19:00:13 +0800

    daily sync with script

--- Sources/Basic/Foundation/Date+DTBKit.swift
+++ Sources/Basic/Foundation/Date+DTBKit.swift
@@ -25,9 +25,19 @@ extension DTBKitStaticWrapper where T == Date {
 
 extension DTBKitWrapper where Base == Date {
     
+    /// Get Int64 by .timeIntervalSince1970
+    public func s() -> Int64 {
+        return Int64(me.timeIntervalSince1970)
+    }
+    
     /// Get timeStamp(ms, length == 13) | 获取 13 位毫秒时间戳
     public func ms() -> Int64 {
         return Int64(me.timeIntervalSince1970 * 1000)
+    }
+    
+    ///
+    public func toString(_ formatter: @autoclosure (() -> DateFormatter)) -> DTBKitWrapper<String> {
+        return formatter().string(from: me).dtb
     }
     
     ///
@@ -35,10 +45,29 @@ extension DTBKitWrapper where Base == Date {
         return DateFormatter().dtb.dateFormat(str).value.string(from: me)
     }
     
-    /// 动态(分段)时间转换
+    /// Dynamic format string | 动态(分段)时间转换
     ///
-    /// Example in Uni-Test
-    /// More details in ``dtbkit_explain.md``
+    /// Defaults result like:
+    /// ```
+    /// 刚刚
+    /// 3分钟前
+    /// 今天 16:59
+    /// 昨天 18:06
+    /// 10-09 18:06
+    /// ```
+    /// More details in ``dtbkit_explain.md``.
+    ///
+    /// - Note: Your custom barrier must cover all situations.
+    /// - Parameters:
+    ///   - barrier: 
+    ///     Mapping relationship from the past to the future |
+    ///     按从过去到未来顺序的时间-字符串映射关系
+    ///   - baseOn:
+    ///     Time axis origin |
+    ///     时间坐标轴原点
+    /// - Returns:
+    ///     Dynamic string depending on time |
+    ///     按规则返回不同样式的字符串
     public func toDynamic(
         _ barrier: [DTB.DateDynamicBarrierItem] = [
             .negative(),
@@ -58,74 +87,66 @@ extension DTBKitWrapper where Base == Date {
         }()
     }
     
-    ///
-    public func toString(_ formatter: @autoclosure (() -> DateFormatter)) -> DTBKitWrapper<String> {
-        return formatter().string(from: me).dtb
+    // MARK: - Calendar
+    
+    /// Self - date | 日期差值
+    public func delta(to date: Date, _ unit: Calendar.Component) -> Int? {
+        return Calendar.current.dateComponents([unit], from: date, to: me).value(for: unit)
+    }
+    
+    /// Use current calendar to check self is same unit by date | 是否为同一 "unit", 注意一般需从年往下逼近判断
+    public func same(to date: Date?, _ unit: Set<Calendar.Component>) -> Bool {
+        guard let d = date else { return false }
+        let l1 = Calendar.current.dateComponents(unit, from: d)
+        let l2 = Calendar.current.dateComponents(unit, from: me)
+        return unit.allSatisfy { l1.value(for: $0) == l2.value(for: $0) }
     }
     
     /// 是否为同一天
-    public func isSameDay(_ date: Date = Date()) -> Bool {
-        let calendar = Calendar.current
-        let unit: Set<Calendar.Component> = [.day, .month, .year]
-        let dateComponents = calendar.dateComponents(unit, from: date)
-        let selfComponents = calendar.dateComponents(unit, from: me)
-        
-        return (dateComponents.year == selfComponents.year) &&
-            (dateComponents.month == selfComponents.month) &&
-            (dateComponents.day == selfComponents.day)
+    public func isSameDay(_ date: Date? = Date()) -> Bool {
+        return same(to: date, [.day, .month, .year])
     }
     
     /// 是否为同一月
-    public func isSameMonth(_ date: Date = Date()) -> Bool {
-        let calendar = Calendar.current
-        let unit: Set<Calendar.Component> = [.month, .year]
-        let dateComponents = calendar.dateComponents(unit, from: date)
-        let selfComponents = calendar.dateComponents(unit, from: me)
-        
-        return (dateComponents.year == selfComponents.year) &&
-            (dateComponents.month == selfComponents.month)
+    public func isSameMonth(_ date: Date? = Date()) -> Bool {
+        return same(to: date, [.month, .year])
     }
     
     /// 是否为同一年
-    public func isSameYear(_ date: Date = Date()) -> Bool {
-        let calendar = Calendar.current
-        let unit: Set<Calendar.Component> = [.year]
-        let dateComponents = calendar.dateComponents(unit, from: date)
-        let selfComponents = calendar.dateComponents(unit, from: me)
-        
-        return dateComponents.year == selfComponents.year
+    public func isSameYear(_ date: Date? = Date()) -> Bool {
+        return same(to: date, [.year])
     }
     
-    /// 当前日期上增减天数
-    public func plusDay(_ days: Int) -> Date {
-        var components = DateComponents()
-        components.day = days
-        let date = Calendar.current.date(byAdding: components, to: me)
-        return date ?? Date()
+    /// Get components value by current calendar | 自由取值
+    public func value(for unit: Calendar.Component) -> Int? {
+        return Calendar.current.dateComponents([unit], from: me).value(for: unit)
     }
     
-    /// 当前日期上增减周数
-    public func plusWeek(_ weeks: Int) -> Date {
+    /// Adding components value by current calendar | 自由增减
+    public func adding(_ value: Int, unit: Calendar.Component) -> Self? {
         var components = DateComponents()
-        components.weekOfYear = weeks
-        let date = Calendar.current.date(byAdding: components, to: me)
-        return date ?? Date()
+        components.setValue(value, for: unit)
+        return Calendar.current.date(byAdding: components, to: me)?.dtb
     }
     
-    /// 当前日期上增减月份
-    public func plusMonth(_ months: Int) -> Date {
-        var components = DateComponents()
-        components.month = months
-        let date = Calendar.current.date(byAdding: components, to: me)
-        return date ?? Date()
+    /// 增减天数
+    public func addingDay(_ days: Int) -> Self? {
+        return adding(days, unit: .day)
     }
     
-    /// 当前日期上增减年
-    public func plusYear(_ years: Int) -> Date {
-        var components = DateComponents()
-        components.year = years
-        let date = Calendar.current.date(byAdding: components, to: me)
-        return date ?? Date()
+    /// 增减周数
+    public func addingWeek(_ weeks: Int) -> Self? {
+        return adding(weeks, unit: .weekOfYear)
+    }
+    
+    /// 增减月份
+    public func addingMonth(_ months: Int) -> Self? {
+        return adding(months, unit: .month)
+    }
+    
+    /// 增减年
+    public func addingYear(_ years: Int) -> Self? {
+        return adding(years, unit: .year)
     }
 }
 
@@ -146,7 +167,7 @@ extension DTB.DateDynamicBarrierItem {
     public static func negative(_ format: String = "yyyy-MM-dd HH:mm") -> Self {
         return .init { base, to in
             let delta = base.timeIntervalSince1970 - to.timeIntervalSince1970
-            return delta < 0 ? to.dtb.format() : nil
+            return delta < 0 ? to.dtb.format(format) : nil
         }
     }
     
@@ -172,14 +193,14 @@ extension DTB.DateDynamicBarrierItem {
     /// 昨天 | "昨天 HH:mm"
     public static func yesterday(_ format: String = "HH:mm") -> Self {
         return .init { base, to in
-            return to.dtb.isSameDay(base.dtb.plusDay(-1)) ? "昨天 \(to.dtb.format(format))" : nil
+            return to.dtb.isSameDay(base.dtb.addingDay(-1)) ? "昨天 \(to.dtb.format(format))" : nil
         }
     }
     
     /// 明天 | "明天 HH:mm"
     public static func tomorrow(_ format: String = "HH:mm") -> Self {
         return .init { base, to in
-            return to.dtb.isSameDay(base.dtb.plusDay(1)) ? "明天 \(to.dtb.format("HH:mm"))" : nil
+            return to.dtb.isSameDay(base.dtb.addingDay(1)) ? "明天 \(to.dtb.format("HH:mm"))" : nil
         }
     }
     
--- dtbkit_explain.md
+++ dtbkit_explain.md
@@ -2,6 +2,15 @@
 
 
 
+> 有些 API 的用法和设计思路需要更细致的说明，而有可能
+>
+> * 有多个同一类设计的 API，每个 API 都写注释不合适；
+> * 代码示例放在注释中不够直观；
+>
+> 所以有了 "如何理解" 系列，作为对代码注释的丰富和补充。
+
+
+
 #### DTB.ConstKey
 
 首先，你需要学会使用并通读 [DefaultsKit](https://github.com/nmdias/DefaultsKit) 源码，在此基础上，可以观察到
@@ -83,6 +92,115 @@ public func dequeueReusableCell<T: UITableViewCell>(_ indexPath: IndexPath) -> T
 
 
 
+#### Date: toDynamic
 
+需求：当前时间相对于给定的时间，过去了多久；举个例子，输出结果有可能是下列之一：
 
- 
+```shell
+刚刚
+3分钟前
+今天 16:59
+昨天 18:06
+10-09 18:06
+2023-10-16 18:06
+```
+
+一个典型的业务实现：
+
+```swift
+public extension Date {
+	/// 动态时间显示
+    var toDynamicString: String {
+        let currentDate = Date()
+        let interval = currentDate.timeIntervalSince1970 - timeIntervalSince1970
+        if interval < 0 {
+            return toString()
+        }
+        if interval < 60 {
+            return "刚刚"
+        }
+        if interval < 3600 {
+            return "\(Int(interval / 60))分钟前"
+        }
+        
+        let dateString = toString(with: "yyyy-MM-dd")
+        let currentDayString = currentDate.toString(with: "yyyy-MM-dd")
+        if dateString == currentDayString {
+            return "今天 \(toString(with: "HH:mm"))"
+        }
+        let yeaterdayString = currentDate.addingDays(-1).toString(with: "yyyy-MM-dd")
+        if dateString == yeaterdayString {
+            return "昨天 \(toString(with: "HH:mm"))"
+        }
+        if currentDate.toString(with: "yyyy") == toString(with: "yyyy") {
+            return toString(with: "MM-dd HH:mm")
+        }
+        return toString()
+    }
+}
+```
+
+实现正常，但这个需求有茫茫多的变体，无法通用。略去思考过程，直接说思路：
+
+* 将时间视为从过去到未来的单向坐标轴，原点是现在；
+* 时间轴可以根据业务，随意切分成有限的几段，显然，各个线段之间不会重叠；
+* 这时，可以建立一对一映射，将每一个片段转换为对应的字符串，而每个转换方法只需要考虑自己的判断条件是否符合，无需通盘考虑其他情况。
+
+把接口雏形写出来：
+
+```swift
+public func toDynamic(_ barrier: [((Date) -> String?)]) -> String? {
+    return barrier.first(where: { $0(me) != nil })?(me)
+}
+```
+
+很容易看出来，barrier 数组从最早的过去（负无穷）开始，向着最远的未来（正无穷）依次判断；将之前的业务代码判断转写成 barrier，就可以实现相同的效果：
+
+```swift
+let myBarrier: [((Date) -> String?)] = [
+	{ to in
+        let delta = Date().timeIntervalSince1970 - to.timeIntervalSince1970
+        return delta < 60 ? "刚刚" : nil
+    }(),
+    { to in
+        let delta = Date().timeIntervalSince1970 - to.timeIntervalSince1970
+        return delta < 3600 ? "\(Int(delta / 60))分钟前" : nil
+    }(),
+    // etc...
+    { to in
+        return to.toString()
+    }()
+]
+```
+
+这样，我们建立了 ``整条时间轴 => [barrier] => [String]``的映射关系；每个业务可以撰写自己的 barrier，而无需调整 ``toDynamic`` 方法本身。接下来，只需要基于雏形代码进行细化：
+
+* 返回非可选值并加以断言拦截，因为 barrier 永远需要覆盖到整条时间轴；
+* 让坐标轴原点可以自由调整；
+* 增加语法糖方便调用。
+
+最终，如果你需要定义自己的映射，创建自己的 ``DateDynamicBarrierItem`` 即可；``baseOn`` 参数表示坐标轴原点，默认是当前时间 ``Date()``：
+
+```swift
+extension DTB.DateDynamicBarrierItem {
+    /// 一分钟内 | "刚刚"
+    public static let oneMinute = Self { base, to in
+        let delta = base.timeIntervalSince1970 - to.timeIntervalSince1970
+        return delta < 60 ? "刚刚" : nil
+    }
+}
+```
+
+业务方的调用代码类似于：
+
+```swift
+timeLabel.text = model.createDate?.dtb.plusWeek(-1).toDynamic()
+
+static let myRule: [DTB.DateDynamicBarrierItem] = [
+    .myPass("EEE HH:mm"), 
+    .myTime01, 
+    .myTime02, .myFuture()
+]
+date.dtb.toDynamic(MyManager.myRule)
+```
+
